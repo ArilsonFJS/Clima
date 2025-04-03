d14ca9aa7698f78693e087f869b5faf69463d504
ArilsonFJS <arilsonsilva61@gmail.com>
2025-04-03 17:55:37 -0300

    configurando button e texfield

--- Clima/Views/ViewController.swift
+++ Clima/Views/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
     
-    private lazy var searchTextFiel: UITextField = {
+    private lazy var searchTextField: UITextField = {
         let searchTextFiel = UITextField()
         searchTextFiel.translatesAutoresizingMaskIntoConstraints = false
         return searchTextFiel
@@ -41,9 +41,51 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        searchTextFiel.delegate = self
+        view.backgroundColor = .white
+        searchTextField.delegate = self
+        setupUI()
     }
 
-
+    func setupUI(){
+        view.addSubview(searchTextField)
+        configSearchTextField()
+        view.addSubview(buttonSearch)
+        configButtonSearch()
+        setContraints()
+    }
+    
+    func configSearchTextField(){
+        searchTextField.borderStyle = .roundedRect
+        searchTextField.placeholder = "Search "
+        searchTextField.textAlignment = .right
+        searchTextField.font = UIFont.systemFont(ofSize: 20)
+    }
+    
+    func configButtonSearch(){
+        let icon = UIImage(systemName: "magnifyingglass")
+        buttonSearch.setImage(icon, for: .normal)
+        
+        buttonSearch.imageView?.contentMode = .scaleAspectFit
+        buttonSearch.contentHorizontalAlignment = .fill
+        buttonSearch.contentVerticalAlignment = .fill
+        
+        
+    }
+    
+    func setContraints(){
+        
+        NSLayoutConstraint.activate([
+            searchTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            searchTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            searchTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -70)
+        ])
+        
+        NSLayoutConstraint.activate([
+            buttonSearch.centerYAnchor.constraint(equalTo: searchTextField.centerYAnchor),
+            buttonSearch.leadingAnchor.constraint(equalTo: searchTextField.trailingAnchor, constant: 15),
+            buttonSearch.heightAnchor.constraint(equalTo: searchTextField.heightAnchor),
+            buttonSearch.widthAnchor.constraint(equalTo: buttonSearch.heightAnchor)
+        ])
+    }
 }
 
