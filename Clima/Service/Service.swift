d3247755a8e7f6a91a32f981f9f80d2dae04b9bf
ArilsonFJS <arilsonsilva61@gmail.com>
2025-04-16 14:57:01 -0300

    implementando metodo para requisicoes https

--- Clima/Service/Service.swift
+++ Clima/Service/Service.swift
@@ -11,7 +11,20 @@ class Service {
     
     func fetchWeather(cityName: String) {
         let urlString = "\(APIConstants.baseUrl)\(cityName)&appid=\(APIConstants.apiKey)"
-        
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        //1. Criar uma URL
         guard let url = URL(string: urlString) else { return }
+        
+        //2. Criar uma URLSession e dar uma tarefa(task) a sessao
+        let session = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            
+        }.resume()//3. Iniciar tarefa(task)
+        
+        
+        
     }
 }
